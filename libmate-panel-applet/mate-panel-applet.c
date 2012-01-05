4b23e8a1c2ddd97c200197abd10e7a6604643469
Stefano Karapetsas <stefano@karapetsas.com>
2012-01-05 16:29:12 +0100

    libmate-panel-applet: Add introspection annotations mate_panel_applet_factory_main was being generated with introspectable="0" because of missing annotations http://git.gnome.org/browse/gnome-panel/commit/?h=gnome-2-32&id=9cbae555115013bc7946e2ce3584d40639248f14

--- libmate-panel-applet/mate-panel-applet.c
+++ libmate-panel-applet/mate-panel-applet.c
@@ -2245,6 +2245,16 @@ _mate_panel_applet_setup_x_error_handler (void)
 	_x_error_func = XSetErrorHandler (_x_error_handler);
 }
 
+/**
+ * mate_panel_applet_factory_main:
+ * @factory_id: Factory ID.
+ * @out_process: If the factory is on a separate process or not.
+ * @applet_type: GType of the applet this factory creates.
+ * @callback: (scope call): Callback to be called when a new applet is to be created.
+ * @data: (closure): Callback data.
+ *
+ * Returns: 0 on success, 1 if there is an error.
+ */
 int mate_panel_applet_factory_main(const gchar* factory_id, gboolean out_process, GType applet_type, MatePanelAppletFactoryCallback callback, gpointer user_data)
 {
 	MatePanelAppletFactory* factory;
