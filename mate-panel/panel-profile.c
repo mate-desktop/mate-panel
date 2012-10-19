e73116a9ea9bafa856a38601cfe90a1bf12e3628
Stefano Karapetsas <stefano@karapetsas.com>
2012-10-19 14:31:30 +0200

    fix load background type at panel start close https://github.com/mate-desktop/mate-panel/issues/49

--- mate-panel/panel-profile.c
+++ mate-panel/panel-profile.c
@@ -222,8 +222,8 @@ panel_profile_set_background_type (PanelToplevel       *toplevel,
 PanelBackgroundType
 panel_profile_get_background_type (PanelToplevel *toplevel)
 {
-	PanelBackgroundType background_type = PANEL_BACK_NONE;
-	g_settings_get_enum (toplevel->background_settings,
+	PanelBackgroundType background_type;
+	background_type = g_settings_get_enum (toplevel->background_settings,
 						 "type");
 	return background_type;
 }
