6df88f1083d89a62b389d2dbb2f95d2e54f019d6
Steve Zesch <stevezesch2@gmail.com>
2012-11-18 20:42:35 -0500

    fixed incorrect fsf addresses.

--- applets/clock/system-timezone.c
+++ applets/clock/system-timezone.c
@@ -16,7 +16,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
  * 
  * Some code is based on previous code in clock-location.c and on code from
  * tz.c (shipped with version <= 2.22.0). Those files were under the same
