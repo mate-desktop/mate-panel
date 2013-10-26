a7a5e35d8a5418d9b9939f6de99367f72ded7b6d
Stefano Karapetsas <stefano@karapetsas.com>
2013-10-26 15:21:44 +0200

    Fix gtkx.h include for GTK2

--- mate-panel/libmate-panel-applet-private/mate-panel-applet-container.c
+++ mate-panel/libmate-panel-applet-private/mate-panel-applet-container.c
@@ -21,7 +21,10 @@
  */
 
 #include <string.h>
+#include <gtk/gtk.h>
+#if GTK_CHECK_VERSION (3, 0, 0)
 #include <gtk/gtkx.h>
+#endif
 #include "mate-panel-applet-container.h"
 #include "panel-marshal.h"
 
