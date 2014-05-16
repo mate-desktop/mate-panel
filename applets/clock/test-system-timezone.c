4534d11d0f99bc0f232f32db48aa87571f61a03c
infirit <infirit@gmail.com>
2014-05-16 21:28:35 +0200

    Drop support for glib < 2.36

--- applets/clock/test-system-timezone.c
+++ applets/clock/test-system-timezone.c
@@ -93,10 +93,6 @@ main (int    argc,
 
 	retval = 0;
 
-#if !GLIB_CHECK_VERSION (2, 36, 0)
-	g_type_init ();
-#endif
-
 	context = g_option_context_new ("");
 	g_option_context_add_main_entries (context, options, NULL);
 
