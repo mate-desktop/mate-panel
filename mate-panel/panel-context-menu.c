92f8b13c967dd867455acc9cdbc5f58a6550b9ae
Stefano Karapetsas <stefano@karapetsas.com>
2014-09-22 11:14:46 +0200

    Add copyright about MATE developers on about window

--- mate-panel/panel-context-menu.c
+++ mate-panel/panel-context-menu.c
@@ -139,7 +139,8 @@ panel_context_menu_show_about_dialog (GtkWidget *menuitem)
 	g_object_set (about,
 		      "program-name",  _("The MATE Panel"),
 		      "version", VERSION,
-		      "copyright", "Copyright \xc2\xa9 1997-2003 Free Software Foundation, Inc.",
+		      "copyright", "Copyright \xc2\xa9 1997-2003 Free Software Foundation, Inc.\n"
+		                   "Copyright \xc2\xa9 2011-2014 MATE Developers.",
 		      "comments", _("This program is responsible for launching other "
 				    "applications and provides useful utilities."),
 		      "authors", authors,
