a641042b9f701c04daa06155323c0f1472e3731c
Denis Gorodnichev <denis.gorodnichev@gmail.com>
2015-02-08 23:25:17 +0300

    na: gtk2: fix build

--- applets/notification_area/na-tray.c
+++ applets/notification_area/na-tray.c
@@ -89,6 +89,27 @@ static TraysScreen *trays_screens = NULL;
 
 static void icon_tip_show_next (IconTip *icontip);
 
+#if !GTK_CHECK_VERSION (3, 0, 0)
+/* NaBox, an instantiable GtkBox */
+
+typedef GtkBox      NaBox;
+typedef GtkBoxClass NaBoxClass;
+
+static GType na_box_get_type (void);
+
+G_DEFINE_TYPE (NaBox, na_box, GTK_TYPE_BOX)
+
+static void
+na_box_init (NaBox *box)
+{
+}
+
+static void
+na_box_class_init (NaBoxClass *klass)
+{
+}
+#endif
+
 /* NaTray */
 
 G_DEFINE_TYPE (NaTray, na_tray, GTK_TYPE_BIN)
