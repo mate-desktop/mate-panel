cce42dab246f4db3daff3eaf56e285091046e5e0
yetist <xiaotian.wu@i-soft.com.cn>
2015-06-18 23:33:03 +0800

    Gtk3: Don't use deprecated gdk_cursor_new

--- mate-panel/panel-widget.c
+++ mate-panel/panel-widget.c
@@ -2038,9 +2038,13 @@ panel_widget_applet_drag_start (PanelWidget *panel,
 		GdkDevice *pointer;
 		GdkDeviceManager *device_manager;
 
-		fleur_cursor = gdk_cursor_new (GDK_FLEUR);
-
 		display = gdk_window_get_display (window);
+#if GTK_CHECK_VERSION(3, 16, 0)
+		fleur_cursor = gdk_cursor_new_for_display (display, GDK_FLEUR);
+#else
+		fleur_cursor = gdk_cursor_new (GDK_FLEUR);
+#endif
+
 		device_manager = gdk_display_get_device_manager (display);
 		pointer = gdk_device_manager_get_client_pointer (device_manager);
 		status = gdk_device_grab (pointer, window,
