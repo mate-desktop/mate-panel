mate-desktop/mate-panel

commit 6f634c680fbc5ee5051253554f2710e39f9ea80a

Don't set the panel transparent by default
This fixes the background for GTK+ 3 themes which don't support mate-panel explicitly. Themes could still overwrite the background if they want in the usual way.

libmate-panel-applet/mate-panel-applet.c
@@ -1979,33 +1979,39 @@ mate_panel_applet_setup (MatePanelApplet *applet)
 void _mate_panel_applet_apply_css(GtkWidget* widget, MatePanelAppletBackgroundType type)
 {
 	GtkStyleContext* context;
-	GtkCssProvider  *provider;
 
 	context = gtk_widget_get_style_context (widget);
-	gtk_widget_reset_style(widget);
+	gtk_widget_reset_style (widget);
 
 	switch (type) {
 	case PANEL_NO_BACKGROUND:
-		gtk_style_context_remove_class(context,"mate-custom-panel-background");
+		gtk_style_context_remove_class (context, "mate-custom-panel-background");
 		break;
 	case PANEL_COLOR_BACKGROUND:
 	case PANEL_PIXMAP_BACKGROUND:
-		provider = gtk_css_provider_new ();
-		gtk_css_provider_load_from_data (provider,
-						".mate-custom-panel-background{\n"
-						" background-color: rgba (0, 0, 0, 0);\n"
-						" background-image: none;\n"
-						"}",
-						-1, NULL);
 		gtk_style_context_add_class (context, "mate-custom-panel-background");
-		gtk_style_context_add_provider (context,
-						GTK_STYLE_PROVIDER (provider),
-						GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
 		break;
 	default:
 		g_assert_not_reached ();
 		break;
 	}
+}
+
+void _mate_panel_applet_prepare_css (GtkStyleContext *context)
+{
+	GtkCssProvider  *provider;
+
+	provider = gtk_css_provider_new ();
+	gtk_css_provider_load_from_data (provider,
+					 ".mate-custom-panel-background{\n"
+					 " background-color: rgba (0, 0, 0, 0);\n"
+					 " background-image: none;\n"
+					 "}",
+					 -1, NULL);
+	gtk_style_context_add_provider (context,
+					GTK_STYLE_PROVIDER (provider),
+					GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+	g_object_unref (provider);
 }
 #endif
 
@@ -2045,10 +2051,10 @@ mate_panel_applet_init (MatePanelApplet *applet)
 	gtk_widget_set_visual(GTK_WIDGET(applet->priv->plug), visual);
 	GtkStyleContext *context;
 	context = gtk_widget_get_style_context (GTK_WIDGET(applet->priv->plug));
-	gtk_style_context_remove_class (context,GTK_STYLE_CLASS_BACKGROUND);
 	gtk_style_context_add_class(context,"gnome-panel-menu-bar");
 	gtk_style_context_add_class(context,"mate-panel-menu-bar");
 	gtk_widget_set_name(GTK_WIDGET(applet->priv->plug), "PanelPlug");
+	_mate_panel_applet_prepare_css(context);
 #endif
 	g_signal_connect_swapped (G_OBJECT (applet->priv->plug), "embedded",
 				  G_CALLBACK (mate_panel_applet_setup),

mate-panel/panel-background.c
@@ -97,13 +97,34 @@ set_pixbuf_background (PanelBackground *background)
 }
 
 #if GTK_CHECK_VERSION (3, 0, 0)
-void panel_background_apply_css (GtkWidget* widget)
+void panel_background_apply_css (GtkWidget* widget, PanelBackground *background)
 {
-	GtkStyleContext* context;
-	GtkCssProvider  *provider;
+	GtkStyleContext     *context;
+	PanelBackgroundType  effective_type;
 
 	context = gtk_widget_get_style_context (widget);
-	gtk_widget_reset_style(widget);
+	effective_type = panel_background_effective_type (background);
+	gtk_widget_reset_style (widget);
+
+	switch (effective_type) {
+	case PANEL_BACK_NONE:
+		gtk_style_context_remove_class (context, "mate-custom-panel-background");
+		break;
+	case PANEL_BACK_COLOR:
+	case PANEL_BACK_IMAGE:
+		gtk_style_context_add_class (context, "mate-custom-panel-background");
+		break;
+	default:
+		g_assert_not_reached ();
+		break;
+	}
+}
+
+static void
+panel_background_prepare_css ()
+{
+	GtkCssProvider      *provider;
+
 	provider = gtk_css_provider_new ();
 	gtk_css_provider_load_from_data (provider,
 					 ".mate-custom-panel-background{\n"
@@ -111,10 +132,10 @@ void panel_background_apply_css (GtkWidget* widget)
 					 " background-image: none;\n"
 					 "}",
 					 -1, NULL);
-	gtk_style_context_add_class (context, "mate-custom-panel-background");
-	gtk_style_context_add_provider (context,
-					GTK_STYLE_PROVIDER (provider),
-					GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+	gtk_style_context_add_provider_for_screen (gdk_screen_get_default (),
+						   GTK_STYLE_PROVIDER (provider),
+						   GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+	g_object_unref (provider);
 }
 #endif
 
@@ -221,6 +242,9 @@ panel_background_prepare (PanelBackground *background)
 				  (gpointer) &widget);
 
 	if (GTK_IS_WIDGET (widget)) {
+#if GTK_CHECK_VERSION (3, 0, 0)
+		panel_background_apply_css (gtk_widget_get_toplevel(widget), background);
+#endif
 		gtk_widget_set_app_paintable(widget,TRUE);
 		gtk_widget_queue_draw (widget);
 	}
@@ -1057,10 +1081,7 @@ panel_background_realized (PanelBackground *background,
 		background->gc = gdk_gc_new (window);
 #endif
 #if GTK_CHECK_VERSION(3, 0, 0)
-	GtkWidget* widget;
-	gdk_window_get_user_data (GDK_WINDOW (background->window),
-                (gpointer) &widget);
-	panel_background_apply_css(gtk_widget_get_toplevel(widget));
+	panel_background_prepare_css ();
 #endif
 	panel_background_prepare (background);
 }

mate-panel/mate-panel-applet-frame.c
@@ -640,15 +640,17 @@ mate_panel_applet_frame_change_background (MatePanelAppletFrame    *frame,
 
 	g_return_if_fail (PANEL_IS_WIDGET (parent));
 
-#if !GTK_CHECK_VERSION (3, 0, 0)
 	if (frame->priv->has_handle) {
 		PanelBackground *background;
 
 		background = &PANEL_WIDGET (parent)->background;
+#if GTK_CHECK_VERSION (3, 0, 0)
+		panel_background_apply_css (GTK_WIDGET (frame), background);
+#else
 		panel_background_change_background_on_widget (background,
 							      GTK_WIDGET (frame));
-	}
 #endif
+	}
 
 	MATE_PANEL_APPLET_FRAME_GET_CLASS (frame)->change_background (frame, type);
 }

mate-panel/panel-background.h
@@ -163,7 +163,7 @@ void panel_background_change_background_on_widget (PanelBackground *background,
 						   GtkWidget       *widget);
 
 #else
-void panel_background_apply_css(GtkWidget* widget);
+void panel_background_apply_css(GtkWidget* widget, PanelBackground *background);
 #endif
 
 #endif /* __PANEL_BACKGROUND_H__ */

mate-panel/panel-menu-bar.c
@@ -479,7 +479,7 @@ void panel_menu_bar_popup_menu(PanelMenuBar* menubar, guint32 activate_time)
 void panel_menu_bar_change_background(PanelMenuBar* menubar)
 {
 #if GTK_CHECK_VERSION (3, 0, 0)
-	panel_background_apply_css(GTK_WIDGET(menubar));
+	panel_background_apply_css(GTK_WIDGET(menubar), &menubar->priv->panel->background);
 #else
 	panel_background_change_background_on_widget(&menubar->priv->panel->background, GTK_WIDGET(menubar));
 #endif

mate-panel/panel-separator.c
@@ -350,7 +350,7 @@ void
 panel_separator_change_background (PanelSeparator *separator)
 {
 #if GTK_CHECK_VERSION (3, 0, 0)
-	panel_background_apply_css(GTK_WIDGET(separator));
+	panel_background_apply_css(GTK_WIDGET(separator), &separator->priv->panel->background);
 #else
 	panel_background_change_background_on_widget(&separator->priv->panel->background, GTK_WIDGET(separator));
 #endif

mate-panel/panel-widget.c
@@ -1657,6 +1657,7 @@ panel_widget_style_set (GtkWidget *widget, GtkStyle  *previous_style)
 		state = gtk_widget_get_state_flags (widget);
 		gtk_style_context_add_class(context,"gnome-panel-menu-bar");
 		gtk_style_context_add_class(context,"mate-panel-menu-bar");
+		panel_background_apply_css (widget, &PANEL_WIDGET (widget)->background);
 
 		gtk_style_context_get_background_color (context, state, &bg_color);
 		gtk_style_context_get (context, state, "background-image", &bg_image, NULL);

commit 2655fb52147cf2426590d375a6fccc97b7997bf3

gtk3: fix infinite loop with Traditional themes
We don't need to reset style when adding/removing classes. It was a regression in commit: 6f634c680fbc5ee5051253554f2710e39f9ea80a

libmate-panel-applet/mate-panel-applet.c
@@ -1981,7 +1981,6 @@ void _mate_panel_applet_apply_css(GtkWidget* widget, MatePanelAppletBackgroundTy
 	GtkStyleContext* context;
 
 	context = gtk_widget_get_style_context (widget);
-	gtk_widget_reset_style (widget);
 
 	switch (type) {
 	case PANEL_NO_BACKGROUND:

mate-panel/panel-background.c
@@ -104,7 +104,6 @@ void panel_background_apply_css (GtkWidget* widget, PanelBackground *background)
 
 	context = gtk_widget_get_style_context (widget);
 	effective_type = panel_background_effective_type (background);
-	gtk_widget_reset_style (widget);
 
 	switch (effective_type) {
 	case PANEL_BACK_NONE: