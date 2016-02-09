975c05e2933bede47cc825b013c626324f0a1133
Wolfgang Ulbrich <chat-to-me@raveit.de>
2016-02-09 13:09:37 +0100

    Kill ubuntu counterhack

--- mate-panel/panel-toplevel.c
+++ mate-panel/panel-toplevel.c
@@ -4895,14 +4895,6 @@ panel_toplevel_init (PanelToplevel *toplevel)
 {
 	int i;
 
-	/* This is a hack for the default resize grip on Ubuntu.
-	 * We need to add a --enable-ubuntu for this.
-	 * Now, it is also default in GTK3.
-	 */
-#if defined(UBUNTU) || GTK_CHECK_VERSION (3, 0, 0)
-	gtk_window_set_has_resize_grip(&toplevel->window_instance, FALSE);
-#endif
-
 	toplevel->priv = PANEL_TOPLEVEL_GET_PRIVATE (toplevel);
 
 	toplevel->priv->expand          = TRUE;
