mate-desktop/mate-panel

commit 882f8f4d9959b6b27f08d2ddb525c7347df1797c

use gtk_box_new instead of gtk_{h,v}box_new properly

applets/clock/calendar-window.c
@@ -43,9 +43,9 @@
 
 #define KEY_LOCATIONS_EXPANDED      "expand-locations"
 
-#if GTK_CHECK_VERSION (3, 0, 0)
-#define gtk_vbox_new(X, Y) gtk_box_new(GTK_ORIENTATION_VERTICAL, Y)
-#define gtk_hbox_new(X, Y) gtk_box_new(GTK_ORIENTATION_HORIZONTAL, Y)
+#if !GTK_CHECK_VERSION (3, 0, 0)
+#define gtk_box_new(GTK_ORIENTATION_HORIZONTAL, b) gtk_hbox_new(FALSE, b)
+#define gtk_box_new(GTK_ORIENTATION_VERTICAL, b) gtk_vbox_new(FALSE, b)
 #endif
 
 enum {
@@ -166,14 +166,14 @@ create_hig_frame (CalendarWindow *calwin,
 	char      *text;
         GtkWidget *expander;
 
-        vbox = gtk_vbox_new (FALSE, 6);
+        vbox = gtk_box_new (GTK_ORIENTATION_VERTICAL, 6);
 
         bold_title = g_strdup_printf ("<b>%s</b>", title);
 	expander = gtk_expander_new (bold_title);
         g_free (bold_title);
 	gtk_expander_set_use_markup (GTK_EXPANDER (expander), TRUE);
 
-	hbox = gtk_hbox_new (FALSE, 0);
+	hbox = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
 	gtk_box_pack_start (GTK_BOX (vbox), hbox, FALSE, FALSE, 0);
         gtk_box_pack_start (GTK_BOX (hbox), expander, FALSE, FALSE, 0);
 	gtk_widget_show_all (vbox);
@@ -252,7 +252,7 @@ calendar_window_fill (CalendarWindow *calwin)
         gtk_container_add (GTK_CONTAINER (calwin), frame);
         gtk_widget_show (frame);
 
-        vbox = gtk_vbox_new (FALSE, 6);
+        vbox = gtk_box_new (GTK_ORIENTATION_VERTICAL, 6);
         gtk_container_set_border_width (GTK_CONTAINER (vbox), 6);
         gtk_container_add (GTK_CONTAINER (frame), vbox);
         gtk_widget_show (vbox);

applets/clock/clock-location-tile.c
@@ -17,14 +17,12 @@
 #include "clock-marshallers.h"
 #include "set-timezone.h"
 
-#if GTK_CHECK_VERSION (3, 0, 0)
-#define gtk_hbox_new(X,Y) gtk_box_new(GTK_ORIENTATION_HORIZONTAL,Y)
-#define gtk_vbox_new(X,Y) gtk_box_new(GTK_ORIENTATION_VERTICAL,Y)
-#endif
 
 #if GTK_CHECK_VERSION (3, 0, 0)
 G_DEFINE_TYPE (ClockLocationTile, clock_location_tile, GTK_TYPE_BIN)
 #else
+#define gtk_box_new(GTK_ORIENTATION_HORIZONTAL, b) gtk_hbox_new(FALSE, b)
+#define gtk_box_new(GTK_ORIENTATION_VERTICAL, b) gtk_vbox_new(FALSE, b)
 G_DEFINE_TYPE (ClockLocationTile, clock_location_tile, GTK_TYPE_ALIGNMENT)
 #endif
 
@@ -290,13 +288,13 @@ clock_location_tile_fill (ClockLocationTile *this)
         gtk_alignment_set_padding (GTK_ALIGNMENT (alignment), 3, 3, 3, 0);
 #endif
 
-        tile = gtk_hbox_new (FALSE, 6);
+        tile = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 6);
 #if GTK_CHECK_VERSION (3, 0, 0)
         gtk_widget_set_margin_top (tile, 3);
         gtk_widget_set_margin_bottom (tile, 3);
         gtk_widget_set_margin_start (tile, 3);
 #endif
-        head_section = gtk_vbox_new (FALSE, 0);
+        head_section = gtk_box_new (GTK_ORIENTATION_VERTICAL, 0);
 
         priv->city_label = gtk_label_new (NULL);
 #if GTK_CHECK_VERSION (3, 0, 0)
@@ -337,7 +335,7 @@ clock_location_tile_fill (ClockLocationTile *this)
         gtk_container_add (GTK_CONTAINER (align), priv->weather_icon);
 #endif
 
-        box = gtk_hbox_new (FALSE, 0);
+        box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
         gtk_box_pack_start (GTK_BOX (head_section), box, FALSE, FALSE, 0);
 #if GTK_CHECK_VERSION (3, 0, 0)
         gtk_box_pack_start (GTK_BOX (box), priv->weather_icon, FALSE, FALSE, 0);

applets/clock/clock-location.c
@@ -22,11 +22,6 @@
 #include "set-timezone.h"
 #include "system-timezone.h"
 
-#if GTK_CHECK_VERSION (3, 0, 0)
-#define gtk_vbox_new(X, Y) gtk_box_new(GTK_ORIENTATION_VERTICAL, Y)
-#define gtk_hbox_new(X, Y) gtk_box_new(GTK_ORIENTATION_HORIZONTAL, Y)
-#endif
-
 G_DEFINE_TYPE (ClockLocation, clock_location, G_TYPE_OBJECT)
 
 typedef struct {

applets/clock/clock.c
@@ -85,10 +85,6 @@
 #define KEY_TEMPERATURE_UNIT	"temperature-unit"
 #define KEY_SPEED_UNIT		"speed-unit"
 
-#if GTK_CHECK_VERSION (3, 0, 0)
-#define gtk_vbox_new(X, Y) gtk_box_new(GTK_ORIENTATION_VERTICAL, Y)
-#endif
-
 enum {
 	COL_CITY_NAME = 0,
 	COL_CITY_TZ,
@@ -1019,7 +1015,11 @@ create_clock_window (ClockData *cd)
         locations_box = calendar_window_get_locations_box (CALENDAR_WINDOW (cd->calendar_popup));
         gtk_widget_show (locations_box);
 
+#if GTK_CHECK_VERSION (3, 0, 0)
+	cd->clock_vbox = gtk_box_new (GTK_ORIENTATION_VERTICAL, 6);
+#else
 	cd->clock_vbox = gtk_vbox_new (FALSE, 6);
+#endif
 	gtk_container_add (GTK_CONTAINER (locations_box), cd->clock_vbox);
 
 	cd->clock_group = gtk_size_group_new (GTK_SIZE_GROUP_HORIZONTAL);
@@ -1168,7 +1168,11 @@ create_cities_section (ClockData *cd)
 		g_list_free (cd->location_tiles);
         cd->location_tiles = NULL;
 
+#if GTK_CHECK_VERSION (3, 0, 0)
+        cd->cities_section = gtk_box_new (GTK_ORIENTATION_VERTICAL, 6);
+#else
         cd->cities_section = gtk_vbox_new (FALSE, 6);
+#endif
         gtk_container_set_border_width (GTK_CONTAINER (cd->cities_section), 0);
 
 	cities = cd->locations;

applets/notification_area/na-tray.c
@@ -34,11 +34,6 @@
 #define ICON_SPACING 1
 #define MIN_BOX_SIZE 3
 
-#if GTK_CHECK_VERSION (3, 0, 0)
-#define gtk_vbox_new(X, Y) gtk_box_new(GTK_ORIENTATION_VERTICAL, Y)
-#define gtk_hbox_new(X, Y) gtk_box_new(GTK_ORIENTATION_HORIZONTAL, Y)
-#endif
-
 typedef struct
 {
   NaTrayManager *tray_manager;

applets/notification_area/testtray.c
@@ -29,11 +29,6 @@
 
 #define NOTIFICATION_AREA_ICON "mate-panel-notification-area"
 
-#if GTK_CHECK_VERSION (3, 0, 0)
-#define gtk_hbox_new(X,Y) gtk_box_new(GTK_ORIENTATION_HORIZONTAL,Y)
-#define gtk_vbox_new(X,Y) gtk_box_new(GTK_ORIENTATION_VERTICAL,Y)
-#endif
-
 static guint n_windows = 0;
 
 typedef struct
@@ -155,14 +150,22 @@ create_tray_on_screen (GdkScreen *screen,
   data->window = window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
   g_object_weak_ref (G_OBJECT (window), (GWeakNotify) maybe_quit, NULL);
 
+#if GTK_CHECK_VERSION (3, 0, 0)
+  vbox = gtk_box_new (GTK_ORIENTATION_VERTICAL, 6);
+#else
   vbox = gtk_vbox_new (FALSE, 6);
+#endif
   gtk_container_add (GTK_CONTAINER (window), vbox);
 
   button = gtk_button_new_with_mnemonic ("_Add another tray");
   g_signal_connect (button, "clicked", G_CALLBACK (add_tray_cb), data);
   gtk_box_pack_start (GTK_BOX (vbox), button, FALSE, FALSE, 0);
 
+#if GTK_CHECK_VERSION (3, 0, 0)
+  hbox = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 12);
+#else
   hbox = gtk_hbox_new (FALSE, 12);
+#endif
   gtk_box_pack_start (GTK_BOX (vbox), hbox, FALSE, FALSE, 0);
   label = gtk_label_new_with_mnemonic ("_Orientation:");
 #if GTK_CHECK_VERSION (3, 16, 0)

mate-panel/nothing.cP
@@ -6,9 +6,6 @@
 #include <cairo.h>
 #include <gtk/gtk.h>
 #include <gdk/gdkkeysyms.h>
-#if GTK_CHECK_VERSION (3, 0, 0)
-#define gtk_vbox_new(X, Y) gtk_box_new(GTK_ORIENTATION_VERTICAL, Y)
-#endif
 #include <canberra-gtk.h>
 
 #ifdef CA_CHECK_VERSION
@@ -1397,7 +1394,11 @@ start_geginv (void)
 	gtk_widget_set_double_buffered (GTK_WIDGET (geginv_canvas), FALSE);
 	gtk_widget_set_size_request (geginv_canvas, inv_width, inv_height);
 
+#if GTK_CHECK_VERSION (3, 0, 0)
+	vbox = gtk_box_new (GTK_ORIENTATION_VERTICAL, 0);
+#else
 	vbox = gtk_vbox_new (FALSE, 0);
+#endif
 	gtk_container_add (GTK_CONTAINER (geginv), vbox);
 	gtk_box_pack_start (GTK_BOX (vbox), geginv_canvas, TRUE, TRUE, 0);
 

mate-panel/panel-addto.c
@@ -52,11 +52,6 @@
 #include "panel-schemas.h"
 #include "panel-stock-icons.h"
 
-#if GTK_CHECK_VERSION (3, 0, 0)
-#define gtk_vbox_new(X, Y) gtk_box_new(GTK_ORIENTATION_VERTICAL, Y)
-#define gtk_hbox_new(X, Y) gtk_box_new(GTK_ORIENTATION_HORIZONTAL, Y)
-#endif
-
 typedef struct {
 	PanelWidget *panel_widget;
 
@@ -1253,10 +1248,18 @@ panel_addto_dialog_new (PanelWidget *panel_widget)
 	g_signal_connect (dialog->addto_dialog, "destroy",
 			  G_CALLBACK (panel_addto_dialog_destroy), dialog);
 
+#if GTK_CHECK_VERSION (3, 0, 0)
+	inner_vbox = gtk_box_new (GTK_ORIENTATION_VERTICAL, 6);
+#else
 	inner_vbox = gtk_vbox_new (FALSE, 6);
+#endif
 	gtk_box_pack_start (GTK_BOX (dialog_vbox), inner_vbox, TRUE, TRUE, 0);
 
+#if GTK_CHECK_VERSION (3, 0, 0)
+	find_hbox = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 6);
+#else
 	find_hbox = gtk_hbox_new (FALSE, 6);
+#endif
 	gtk_box_pack_start (GTK_BOX (inner_vbox), find_hbox, FALSE, FALSE, 0);
 
 	dialog->label = gtk_label_new_with_mnemonic ("");

commit 29a2534c7f5de5a8dc06d48b2e7c52d7416d6a6e

clock: fix UI issues introduced in 882f8f4

applets/clock/calendar-window.c
@@ -43,11 +43,6 @@
 
 #define KEY_LOCATIONS_EXPANDED      "expand-locations"
 
-#if !GTK_CHECK_VERSION (3, 0, 0)
-#define gtk_box_new(GTK_ORIENTATION_HORIZONTAL, b) gtk_hbox_new(FALSE, b)
-#define gtk_box_new(GTK_ORIENTATION_VERTICAL, b) gtk_vbox_new(FALSE, b)
-#endif
-
 enum {
 	EDIT_LOCATIONS,
 	LAST_SIGNAL
@@ -166,14 +161,23 @@ create_hig_frame (CalendarWindow *calwin,
 	char      *text;
         GtkWidget *expander;
 
+#if GTK_CHECK_VERSION (3, 0, 0)
         vbox = gtk_box_new (GTK_ORIENTATION_VERTICAL, 6);
+#else
+        vbox = gtk_vbox_new (FALSE, 6);
+#endif
 
         bold_title = g_strdup_printf ("<b>%s</b>", title);
 	expander = gtk_expander_new (bold_title);
         g_free (bold_title);
 	gtk_expander_set_use_markup (GTK_EXPANDER (expander), TRUE);
 
+#if GTK_CHECK_VERSION (3, 0, 0)
 	hbox = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
+#else
+	hbox = gtk_hbox_new (FALSE, 0);
+#endif
+
 	gtk_box_pack_start (GTK_BOX (vbox), hbox, FALSE, FALSE, 0);
         gtk_box_pack_start (GTK_BOX (hbox), expander, FALSE, FALSE, 0);
 	gtk_widget_show_all (vbox);
@@ -252,7 +256,12 @@ calendar_window_fill (CalendarWindow *calwin)
         gtk_container_add (GTK_CONTAINER (calwin), frame);
         gtk_widget_show (frame);
 
+#if GTK_CHECK_VERSION (3, 0, 0)
         vbox = gtk_box_new (GTK_ORIENTATION_VERTICAL, 6);
+#else
+        vbox = gtk_vbox_new (FALSE, 6);
+#endif
+
         gtk_container_set_border_width (GTK_CONTAINER (vbox), 6);
         gtk_container_add (GTK_CONTAINER (frame), vbox);
         gtk_widget_show (vbox);

applets/clock/clock-location-tile.c
@@ -21,8 +21,6 @@
 #if GTK_CHECK_VERSION (3, 0, 0)
 G_DEFINE_TYPE (ClockLocationTile, clock_location_tile, GTK_TYPE_BIN)
 #else
-#define gtk_box_new(GTK_ORIENTATION_HORIZONTAL, b) gtk_hbox_new(FALSE, b)
-#define gtk_box_new(GTK_ORIENTATION_VERTICAL, b) gtk_vbox_new(FALSE, b)
 G_DEFINE_TYPE (ClockLocationTile, clock_location_tile, GTK_TYPE_ALIGNMENT)
 #endif
 
@@ -283,18 +281,16 @@ clock_location_tile_fill (ClockLocationTile *this)
         g_signal_connect (priv->box, "leave-notify-event",
                           G_CALLBACK (enter_or_leave_tile), this);
 
-#if !GTK_CHECK_VERSION (3, 0, 0)
-        alignment = gtk_alignment_new (0, 0, 1, 0);
-        gtk_alignment_set_padding (GTK_ALIGNMENT (alignment), 3, 3, 3, 0);
-#endif
-
-        tile = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 6);
 #if GTK_CHECK_VERSION (3, 0, 0)
+        tile = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 6);
         gtk_widget_set_margin_top (tile, 3);
         gtk_widget_set_margin_bottom (tile, 3);
         gtk_widget_set_margin_start (tile, 3);
+#else
+        tile = gtk_hbox_new (FALSE, 6);
+        alignment = gtk_alignment_new (0, 0, 1, 0);
+        gtk_alignment_set_padding (GTK_ALIGNMENT (alignment), 3, 3, 3, 0);
 #endif
-        head_section = gtk_box_new (GTK_ORIENTATION_VERTICAL, 0);
 
         priv->city_label = gtk_label_new (NULL);
 #if GTK_CHECK_VERSION (3, 0, 0)
@@ -308,8 +304,10 @@ clock_location_tile_fill (ClockLocationTile *this)
 #endif
 
 #if GTK_CHECK_VERSION (3, 0, 0)
+        head_section = gtk_box_new (GTK_ORIENTATION_VERTICAL, 0);
         gtk_box_pack_start (GTK_BOX (head_section), priv->city_label, FALSE, FALSE, 0);
 #else
+        head_section = gtk_vbox_new (FALSE, 0);
         align = gtk_alignment_new (0, 0, 0, 0);
         gtk_alignment_set_padding (GTK_ALIGNMENT (align), 0, 0, 0, 3);
         gtk_container_add (GTK_CONTAINER (align), priv->city_label);
@@ -335,11 +333,13 @@ clock_location_tile_fill (ClockLocationTile *this)
         gtk_container_add (GTK_CONTAINER (align), priv->weather_icon);
 #endif
 
+#if GTK_CHECK_VERSION (3, 0, 0)
         box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
         gtk_box_pack_start (GTK_BOX (head_section), box, FALSE, FALSE, 0);
-#if GTK_CHECK_VERSION (3, 0, 0)
         gtk_box_pack_start (GTK_BOX (box), priv->weather_icon, FALSE, FALSE, 0);
 #else
+        box = gtk_hbox_new (FALSE, 0);
+        gtk_box_pack_start (GTK_BOX (head_section), box, FALSE, FALSE, 0);
         gtk_box_pack_start (GTK_BOX (box), align, FALSE, FALSE, 0);
 #endif
         gtk_box_pack_start (GTK_BOX (box), priv->time_label, FALSE, FALSE, 0);