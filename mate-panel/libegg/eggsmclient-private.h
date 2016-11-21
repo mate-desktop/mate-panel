9e5c593552795c283aec5fc61478794e6b2b4ae7
monsta <monsta@inbox.ru>
2016-11-21 18:34:53 +0300

    libegg: remove win32 & quartz leftovers

--- mate-panel/libegg/eggsmclient-private.h
+++ mate-panel/libegg/eggsmclient-private.h
@@ -43,13 +43,7 @@ EggSMClient *egg_sm_client_xsmp_new      (void);
 #ifdef EGG_SM_CLIENT_BACKEND_DBUS
 GType        egg_sm_client_dbus_get_type (void);
 EggSMClient *egg_sm_client_dbus_new      (void);
-# endif
-#elif defined (GDK_WINDOWING_WIN32)
-GType        egg_sm_client_win32_get_type (void);
-EggSMClient *egg_sm_client_win32_new      (void);
-#elif defined (GDK_WINDOWING_QUARTZ)
-GType        egg_sm_client_osx_get_type (void);
-EggSMClient *egg_sm_client_osx_new      (void);
+#endif
 #endif
 
 #ifdef __cplusplus
--- mate-panel/libegg/eggsmclient.c
+++ mate-panel/libegg/eggsmclient.c
@@ -335,11 +335,6 @@ egg_sm_client_get (void)
     {
       if (!sm_client_disable)
 	{
-#if defined (GDK_WINDOWING_WIN32)
-	  global_client = egg_sm_client_win32_new ();
-#elif defined (GDK_WINDOWING_QUARTZ)
-	  global_client = egg_sm_client_osx_new ();
-#else
 	  /* If both D-Bus and XSMP are compiled in, try XSMP first
 	   * (since it supports state saving) and fall back to D-Bus
 	   * if XSMP isn't available.
@@ -350,7 +345,6 @@ egg_sm_client_get (void)
 #ifdef EGG_SM_CLIENT_BACKEND_DBUS
 	  if (!global_client)
 	    global_client = egg_sm_client_dbus_new ();
-# endif
 #endif
 	}
 
