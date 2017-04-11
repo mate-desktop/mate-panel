de0611f1b7d937ce8c316ab8643349875aa80cc5
monsta <monsta@inbox.ru>
2017-04-11 12:06:23 +0300

    panel-menu-bar: fix build warning and some spacing

--- mate-panel/panel-menu-bar.c
+++ mate-panel/panel-menu-bar.c
@@ -352,8 +352,9 @@ static void panel_menu_bar_load(PanelWidget* panel, gboolean locked, int positio
 		gtk_widget_destroy(GTK_WIDGET(menubar));
 		return;
 	}
-	settings = gtk_settings_get_for_screen(gtk_widget_get_screen(panel));
-	g_object_set(settings,"gtk-shell-shows-app-menu",FALSE,"gtk-shell-shows-menubar",FALSE,NULL);
+
+	settings = gtk_settings_get_for_screen (gtk_widget_get_screen (GTK_WIDGET (panel)));
+	g_object_set (settings, "gtk-shell-shows-app-menu", FALSE, "gtk-shell-shows-menubar", FALSE, NULL);
 
 	mate_panel_applet_add_callback(menubar->priv->info, "help", GTK_STOCK_HELP, _("_Help"), NULL);
 
