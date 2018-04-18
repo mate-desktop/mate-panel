7dcc2149608493ce8bc87ee189967854e9fd0033
lukefromdc <lukefromdc@hushmail.com>
2018-04-18 00:40:59 -0400

    Revert "Theme: limit fallback applet drag handle theme to GTK 3.20/3.22"
    This reverts commit 3a8b1f66d2393a35f40dbcc211d3bf045d563a92.
    Apply default applet handle theme in GTK 3.18 and earlier builds

--- mate-panel/libmate-panel-applet-private/panel-applet-frame-dbus.c
+++ mate-panel/libmate-panel-applet-private/panel-applet-frame-dbus.c
@@ -245,10 +245,9 @@ mate_panel_applet_frame_dbus_change_background (MatePanelAppletFrame    *frame,
 	MatePanelAppletFrameDBus *dbus_frame = MATE_PANEL_APPLET_FRAME_DBUS (frame);
 	MatePanelAppletFrameDBusPrivate *priv = dbus_frame->priv;
 	char *bg_str;
-#if GTK_CHECK_VERSION (3, 20, 0)
     gchar *theme_name;
     GtkSettings *settings;
-#endif
+
 	bg_str = _mate_panel_applet_frame_get_background_string (
 			frame, PANEL_WIDGET (gtk_widget_get_parent (GTK_WIDGET (frame))), type);
 
@@ -265,7 +264,6 @@ mate_panel_applet_frame_dbus_change_background (MatePanelAppletFrame    *frame,
 
 		g_free (bg_str);
 	}
-#if GTK_CHECK_VERSION (3, 20, 0)
     GtkCssProvider *provider;
     provider = gtk_css_provider_new ();
 
@@ -327,7 +325,6 @@ mate_panel_applet_frame_dbus_change_background (MatePanelAppletFrame    *frame,
                                     GTK_STYLE_PROVIDER_PRIORITY_FALLBACK);
     g_object_unref (provider);
     g_free (theme_name);
-#endif
 }
 
 static void
