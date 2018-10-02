ec661f0bd3fc4c9dd68f4182d3b6d40f3c686528
Fabien Broquard <braikar@gmail.com>
2018-10-02 20:16:53 +0200

    na-tray: wide panels, preliminary batch box to grid rename
    box/Box/BOX changed to grid/Grid/GRID as a preliminary step
    to prepare for changing GtkBox in na-box/na-grid to a GtkGrid
    to make the notification area work well on vertical and wide panels
    note: even if everything is renamed to grid, the GtkWidget is still a GtkBox
    thats why GtkBox and GTK_TYPE_BOX have not been renamed.

--- applets/notification_area/na-grid.h
+++ applets/notification_area/na-grid.h
@@ -22,8 +22,8 @@
  * Used to be: eggtraytray.h
  */
 
-#ifndef NA_BOX_H
-#define NA_BOX_H
+#ifndef NA_GRID_H
+#define NA_GRID_H
 
 #ifdef GDK_WINDOWING_X11
 #include <gdk/gdkx.h>
@@ -32,25 +32,12 @@
 
 G_BEGIN_DECLS
 
-#define NA_TYPE_BOX			(na_box_get_type ())
-#define NA_BOX(obj)			(G_TYPE_CHECK_INSTANCE_CAST ((obj), NA_TYPE_BOX, NaBox))
-#define NA_BOX_CLASS(klass)		(G_TYPE_CHECK_CLASS_CAST ((klass), NA_TYPE_BOX, NaBoxClass))
-#define NA_IS_BOX(obj)			(G_TYPE_CHECK_INSTANCE_TYPE ((obj), NA_TYPE_BOX))
-#define NA_IS_BOX_CLASS(klass)		(G_TYPE_CHECK_CLASS_TYPE ((klass), NA_TYPE_BOX))
-#define NA_BOX_GET_CLASS(obj)		(G_TYPE_INSTANCE_GET_CLASS ((obj), NA_TYPE_BOX, NaBoxClass))
+#define NA_TYPE_GRID (na_grid_get_type ())
+G_DECLARE_FINAL_TYPE (NaGrid, na_grid, NA, GRID, GtkBox)
 
-typedef struct _NaBox		NaBox;
-typedef struct _NaBoxClass	NaBoxClass;
-
-struct _NaBoxClass
-{
-  GtkBoxClass parent_class;
-};
-
-GType           na_box_get_type         (void);
-GtkWidget      *na_box_new              (GtkOrientation orientation);
-void            na_box_force_redraw     (NaBox *box);
+GtkWidget      *na_grid_new                     (GtkOrientation orientation);
+void            na_grid_force_redraw            (NaGrid *grid);
 
 G_END_DECLS
 
-#endif /* __NA_TRAY_H__ */
+#endif /* __NA_GRID_H__ */
