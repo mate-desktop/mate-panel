517435ea0a864438509102901dbd7e177002e3f3
Laurent Napias <tamplan@free.fr>
2019-06-10 10:54:05 +0200

    Update FSF address

--- mate-panel/panel-a11y.h
+++ mate-panel/panel-a11y.h
@@ -15,7 +15,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Street #330, Boston, MA 02110-1301, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
 #ifndef __PANEL_A11Y_H__
