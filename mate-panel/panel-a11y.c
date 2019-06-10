1b30eea533e97d10bc6b1c23c1592bb0919942ee
Laurent Napias <tamplan@free.fr>
2019-06-10 10:44:37 +0200

    Auto stash before revert of "Update FSF address"

--- mate-panel/panel-a11y.c
+++ mate-panel/panel-a11y.c
@@ -15,7 +15,11 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
+<<<<<<< Updated upstream
  * Foundation, Inc., 59 Temple Street #330, Boston, MA 02110-1301, USA.
+=======
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+>>>>>>> Stashed changes
  */
 
 #include "panel-a11y.h"
