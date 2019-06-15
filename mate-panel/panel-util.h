8cf8c2909aab1b7ea1b32774d03a870f8d2a0d6a
raveit65 <mate@raveit.de>
2019-06-15 11:52:40 +0200

    Revert "menu-bar: transfer focus correctly on alt-F1"
    This reverts commit f0f4c5e1217eefd46edf9f98633fb32967c67461.
    
    Fixes https://github.com/mate-desktop/marco/issues/209

--- mate-panel/panel-util.h
+++ mate-panel/panel-util.h
@@ -3,7 +3,6 @@
 
 #include <gio/gio.h>
 #include <gtk/gtk.h>
-#include <X11/Xlib.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -67,10 +66,6 @@ void panel_util_set_tooltip_text (GtkWidget  *widget,
 
 GFile *panel_util_get_file_optional_homedir (const char *location);
 
-Window panel_util_get_current_active_window (GtkWidget *toplevel);
-
-void   panel_util_set_current_active_window (GtkWidget *toplevel,
-					     Window     window);
 #ifdef __cplusplus
 }
 #endif
